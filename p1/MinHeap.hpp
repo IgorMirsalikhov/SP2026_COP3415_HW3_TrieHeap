453994f79ac6e27443f95a6768332a76cb9391a6
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2026-04-02 15:07:56 -0400

    Add files via upload

---
+++ p1/MinHeap.hpp
@@ -0,0 +1,29 @@
+#ifndef MIN_HEAP_HPP
+#define MIN_HEAP_HPP
+
+#include <vector>
+
+template <typename T>
+class MinHeap {
+public:
+    // Constructors
+    MinHeap();
+    MinHeap(const std::vector<T>& v);
+
+    void insert(const T& val);
+    void print() const;
+
+    T deleteMin();
+private:
+    std::vector<T> data;
+    
+    void heapify();
+    void percolateUp();
+    void percolateDown(int i = 0);
+    int getLeftKidIndex(int i);
+    int getRightKidIndex(int i);
+    int getParentIndex(int i);
+    int getLastWithKidsIndex();
+};
+
+#endif
